457d19e7c577fe9212d1b752c9353e4c5c00ac47
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-09-27 14:32:57 +0200

    fix path

--- VggTools/macros/evtcount.sh
+++ VggTools/macros/evtcount.sh
@@ -2,8 +2,8 @@
 
 WORKDIR=/home/dellaric/work/cms/VggAnalysis/VggTools/macros
 
-for L in `ls $WORKDIR/lists`; do
-  root-6.12 -l -b -q $WORKDIR/evtcount.C\(\"$WORKDIR/lists/$L\"\) | \
+for L in `ls $WORKDIR/../scripts/lists`; do
+  root-6.12 -l -b -q $WORKDIR/evtcount.C\(\"$WORKDIR/../scripts/lists/$L\"\) | \
   grep -v 'Processing'
 done
 
