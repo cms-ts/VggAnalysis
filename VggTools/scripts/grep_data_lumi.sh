14c27c50189564db105ce1bf663313bfd4c3a409
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-09-27 09:49:49 +0200

    add script to grep lumi info

---
+++ VggTools/scripts/grep_data_lumi.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+for F in `ls lumi/Run2016*.lumi | grep Run2016[A-Z]`; do
+  G=`basename $F .lumi`
+  L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
+  printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
+done
+
+echo
+
+for F in `ls lumi/Run2017*.lumi | grep Run2017[A-Z]_`; do
+  G=`basename $F .lumi`
+  L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
+  printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
+done
+
+echo
+
+for F in `ls lumi/Run2016*.lumi | grep -v Run2016[A-Z]_`; do
+  G=`basename $F .lumi`
+  L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
+  printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
+done
+
+echo
+
+for F in `ls lumi/Run2017*.lumi | grep -v Run2017[A-Z]_`; do
+  G=`basename $F .lumi`
+  L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
+  printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
+done
+
+exit
