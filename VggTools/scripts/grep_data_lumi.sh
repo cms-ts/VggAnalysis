cb81e41073f46bec0fc1bad8918b26b6eedb261d
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-09-27 14:30:01 +0200

    add WORKDIR

--- VggTools/scripts/grep_data_lumi.sh
+++ VggTools/scripts/grep_data_lumi.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+WORKDIR=/home/dellaric/work/cms/VggAnalysis/VggTools/scripts
+
+cd $WORKDIR
+
 for F in `ls lumi/Run2016*.lumi | grep Run2016[A-Z]`; do
   G=`basename $F .lumi`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
