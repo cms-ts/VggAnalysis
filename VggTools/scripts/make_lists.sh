743a475255bff8ac67e7763cc83995dd9cfd93c4
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-09-29 09:25:53 +0200

    cosmetics

--- VggTools/scripts/make_lists.sh
+++ VggTools/scripts/make_lists.sh
@@ -5,28 +5,28 @@ WORKDIR=/home/$USER/work/cms/VggAnalysis/VggTools/scripts
 
 cd $WORKDIR
 
-for D1 in `ls $DATADIR | grep Run2016`; do
+for D1 in `ls $DATADIR/ | grep Run2016`; do
   for D2 in `ls $DATADIR/$D1`; do
     find $DATADIR/$D1/$D2 -type f | grep 05Feb2018 | \
     awk '{print "root://eosinfnts.ts.infn.it/"$1}' > lists/${D1}_${D2}_05Feb2018.list
   done
 done
 
-for D1 in `ls $DATADIR | grep Run2016`; do
+for D1 in `ls $DATADIR/ | grep Run2016`; do
   for D2 in `ls $DATADIR/$D1`; do
     find $DATADIR/$D1/$D2 -type f | grep 22Aug2018 | \
     awk '{print "root://eosinfnts.ts.infn.it/"$1}' > lists/${D1}_${D2}_22Aug2018.list
   done
 done
 
-for D1 in `ls $DATADIR | grep Run2017`; do
+for D1 in `ls $DATADIR/ | grep Run2017`; do
   for D2 in `ls $DATADIR/$D1`; do
     find $DATADIR/$D1/$D2 -type f | grep 31Mar2018 | \
     awk '{print "root://eosinfnts.ts.infn.it/"$1}' > lists/${D1}_${D2}_31Mar2018.list
   done
 done
 
-for D1 in `ls $DATADIR | grep Run2018`; do
+for D1 in `ls $DATADIR/ | grep Run2018`; do
   for D2 in `ls $DATADIR/$D1`; do
     find $DATADIR/$D1/$D2 -type f | grep 14Sep2018 | \
     awk '{print "root://eosinfnts.ts.infn.it/"$1}' > lists/${D1}_${D2}_14Sep2018.list
@@ -55,7 +55,7 @@ done
 
 DATADIR=/eos/infnts/cms/store/mc
 
-for D1 in `ls $DATADIR | grep RunII.*NanoAOD`; do
+for D1 in `ls $DATADIR/ | grep RunII.*NanoAOD`; do
   for D2 in `ls $DATADIR/$D1`; do
     find $DATADIR/$D1/$D2 -type f | \
     awk '{print "root://eosinfnts.ts.infn.it/"$1}' > lists/${D1}_${D2}.list
