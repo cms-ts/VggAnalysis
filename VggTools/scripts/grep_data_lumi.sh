f542a2714b920a89108c05712790a4197024d769
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-09-29 09:20:22 +0200

    make sure dataset name not truncated in data.lumi output

--- VggTools/scripts/grep_data_lumi.sh
+++ VggTools/scripts/grep_data_lumi.sh
@@ -10,7 +10,7 @@ for F in `ls lumi/Run2016*.lumi | grep Run2016[A-Z]`; do
   G=`basename $F .lumi`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
   printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
-  printf "%-35s\t%6.3f\n" $G $L >> data.lumi
+  printf "%s\t%6.3f\n" $G $L >> data.lumi
 done
 
 echo
@@ -19,7 +19,7 @@ for F in `ls lumi/Run2017*.lumi | grep Run2017[A-Z]_`; do
   G=`basename $F .lumi`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
   printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
-  printf "%-35s\t%6.3f\n" $G $L >> data.lumi
+  printf "%s\t%6.3f\n" $G $L >> data.lumi
 done
 
 echo
@@ -28,7 +28,7 @@ for F in `ls lumi/Run2018*.lumi | grep Run2018[A-Z]_`; do
   G=`basename $F .lumi`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
   printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
-  printf "%-35s\t%6.3f\n" $G $L >> data.lumi
+  printf "%s\t%6.3f\n" $G $L >> data.lumi
 done
 
 echo
@@ -37,7 +37,7 @@ for F in `ls lumi/Run2016*.lumi | grep -v Run2016[A-Z]_`; do
   G=`basename $F .lumi`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
   printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
-  printf "%-35s\t%6.3f\n" $G $L >> data.lumi
+  printf "%s\t%6.3f\n" $G $L >> data.lumi
 done
 
 echo
@@ -46,14 +46,14 @@ for F in `ls lumi/Run2017*.lumi | grep -v Run2017[A-Z]_`; do
   G=`basename $F .lumi`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
   printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
-  printf "%-35s\t%6.3f\n" $G $L >> data.lumi
+  printf "%s\t%6.3f\n" $G $L >> data.lumi
 done
 
 for F in `ls lumi/Run2018*.lumi | grep -v Run2018[A-Z]_`; do
   G=`basename $F .lumi`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
   printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
-  printf "%-35s\t%6.3f\n" $G $L >> data.lumi
+  printf "%s\t%6.3f\n" $G $L >> data.lumi
 done
 
 exit
