91d5531f4fddbda54cf02321b3181c208eff9a6f
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-09-29 18:09:32 +0200

    change scripts/json/ in scripts/logs/

--- VggTools/scripts/grep_data_lumi.sh
+++ VggTools/scripts/grep_data_lumi.sh
@@ -6,7 +6,7 @@ cd $WORKDIR
 
 rm -f lumi.dat 
 
-for F in `ls json/Run2016*.lumi | grep Run2016[A-Z]`; do
+for F in `ls logs/Run2016*.lumi | grep Run2016[A-Z]`; do
   G=`basename $F .lumi`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
   printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
@@ -15,7 +15,7 @@ done
 
 echo
 
-for F in `ls json/Run2017*.lumi | grep Run2017[A-Z]_`; do
+for F in `ls logs/Run2017*.lumi | grep Run2017[A-Z]_`; do
   G=`basename $F .lumi`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
   printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
@@ -24,7 +24,7 @@ done
 
 echo
 
-for F in `ls json/Run2018*.lumi | grep Run2018[A-Z]_`; do
+for F in `ls logs/Run2018*.lumi | grep Run2018[A-Z]_`; do
   G=`basename $F .lumi`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
   printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
@@ -33,7 +33,7 @@ done
 
 echo
 
-for F in `ls json/Run2016*.lumi | grep -v Run2016[A-Z]_`; do
+for F in `ls logs/Run2016*.lumi | grep -v Run2016[A-Z]_`; do
   G=`basename $F .lumi`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
   printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
@@ -42,14 +42,14 @@ done
 
 echo
 
-for F in `ls json/Run2017*.lumi | grep -v Run2017[A-Z]_`; do
+for F in `ls logs/Run2017*.lumi | grep -v Run2017[A-Z]_`; do
   G=`basename $F .lumi`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
   printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
   printf "%s\t%6.3f\n" $G $L >> lumi.dat
 done
 
-for F in `ls json/Run2018*.lumi | grep -v Run2018[A-Z]_`; do
+for F in `ls logs/Run2018*.lumi | grep -v Run2018[A-Z]_`; do
   G=`basename $F .lumi`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
   printf "dataset %-35s - lumi = %6.3f /fb\n" $G $L
