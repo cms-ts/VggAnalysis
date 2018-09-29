d614f4024bc680a57f11b5fc8f0c4bf7604677c2
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-09-29 13:02:31 +0200

    drop $WORKDIR when useless

--- VggTools/macros/job_selector.sh
+++ VggTools/macros/job_selector.sh
@@ -7,9 +7,9 @@ cd $WORKDIR
 VERSION=$1
 LIST=$2
 
-FILE=$WORKDIR/data/$VERSION/`basename $2 | sed -e 's;.list;;'`.root
-LOG=$WORKDIR/data/$VERSION/`basename $2 | sed -e 's;.list;;'`.log
+FILE=data/$VERSION/`basename $2 | sed -e 's;.list;;'`.root
+LOG=data/$VERSION/`basename $2 | sed -e 's;.list;;'`.log
 
-root-6.12 -l -b -q $WORKDIR/run.C\(\"$LIST\",\"$FILE\"\) > $LOG 2>&1
+root-6.12 -l -b -q run.C\(\"$LIST\",\"$FILE\"\) > $LOG 2>&1
 
 exit
