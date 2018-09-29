40fb99acedff7112834db8dbd6e3eb82dc55362d
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-09-29 09:47:31 +0200

    add script to copy selected lists from scritps/lists/ to macros/lists/

---
+++ VggTools/scripts/copy_lists.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+WORKDIR=/home/$USER/work/cms/VggAnalysis/VggTools/scripts
+
+cd $WORKDIR
+
+LISTS=`ls lists/`
+
+for L in $LISTS; do
+  [ -z "${L##*DoubleEG_22Aug2018*}"       ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*DoubleMuon_22Aug2018*}"     ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*SingleElectron_22Aug2018*}" ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*SingleMuon_22Aug2018*}"     ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*MuonEG_22Aug2018*}"         ] && cp -p lists/$L ../macros/lists/
+done
+
+for L in $LISTS; do
+  [ -z "${L##*DoubleEG_31Mar2018*}"       ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*DoubleMuon_31Mar2018*}"     ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*SingleElectron_31Mar2018*}" ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*SingleMuon_31Mar2018*}"     ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*MuonEG_31Mar2018*}"         ] && cp -p lists/$L ../macros/lists/
+done
+
+for L in $LISTS; do
+  [ -z "${L##*DoubleEG_14Sep2018*}"       ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*DoubleMuon_14Sep2018*}"     ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*SingleElectron_14Sep2018*}" ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*SingleMuon_14Sep2018*}"     ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*MuonEG_14Sep2018*}"         ] && cp -p lists/$L ../macros/lists/
+done
+
+for L in $LISTS; do
+  [ -z "${L##*RunIISummer16*}"            ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*RunIIFall17*}"              ] && cp -p lists/$L ../macros/lists/
+  [ -z "${L##*RunIIFall18*}"              ] && cp -p lists/$L ../macros/lists/
+done
+
+exit
