4df3f6d43e403c75ca86c7570c491f13e6759c1b
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-09-30 11:23:00 +0200

    add code snippet to read lumi & xsec files

--- VggTools/macros/plot0.C
+++ VggTools/macros/plot0.C
@@ -2,6 +2,38 @@
 
 void plot0(string title="") {
 
-cout << title << endl;
+  map<string, float> lumiData;
+
+  ifstream input0("lumi.dat");
+  string line0;
+  while (getline(input0, line0)) {
+    if (line0.length() > 0 && line0[0] != '#') {
+      istringstream stream(line0);
+      string key;
+      float val;
+      stream >> key >> val;
+      lumiData[key] = val;
+//      cout << key << " " << val << endl;
+    }
+  }
+  input0.close();
+  cout << "Read lumi data for " << lumiData.size() << " datasets" << endl;
+
+  map<string, float> xsecData;
+
+  ifstream input1("xsec.dat");
+  string line1;
+  while (getline(input0, line0)) {
+    if (line0.length() > 0 && line0[0] != '#') {
+      istringstream stream(line0);
+      string key;
+      float val;
+      stream >> key >> val;
+      xsecData[key] = val;
+//      cout << key << " " << val << endl;
+    }
+  }
+  input1.close();
+  cout << "Read xsec data for " << xsecData.size() << " datasets" << endl;
 
 }
