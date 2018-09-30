8cd0c26ed2d5d0efdb1e8c72252b2b14c8623e86
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-09-30 16:55:51 +0200

    add dummy xsec.dat & plotMap_Zgg.dat and setup histo reading

--- VggTools/macros/plot0.C
+++ VggTools/macros/plot0.C
@@ -2,6 +2,8 @@
 
 void plot0(string title="") {
 
+  string version = "v00";
+
   map<string, float> lumiMap;
   readMap("lumi.dat", lumiMap);
   cout << "Read lumi map for " << lumiMap.size() << " datasets" << endl;
@@ -14,9 +16,37 @@ void plot0(string title="") {
   readMap("plotMap_Zgg.dat", plotMap);
   cout << "Read plot map for " << plotMap.size() << " datasets" << endl;
 
-  map<string, float>::iterator it;
-  for (it=plotMap.begin(); it!=plotMap.end(); it++) {
-    cout << it->first << " " << it->second << endl;
+  TH1D* histo[999] = {0};
+
+  int ngen = 0;
+  float lumi = 0.0;
+
+  for (map<string, float>::iterator it = plotMap.begin(); it != plotMap.end(); it++) {
+    TFile file(("data/" + version + "/" + it->first + ".root").c_str()); 
+    int index = int(it->second);
+    if (index == 0) {
+      lumi = lumiMap[it->first];
+      histo[index] = (TH1D*)gDirectory->Get(title.c_str());
+      histo[index]->SetDirectory(0);
+    }
+    if (index != 0) {
+      ngen = ((TH1D*)gDirectory->Get("h_nevt"))->GetBinContent(1);
+      float norm = lumi/ngen;
+      if (histo[index]) {
+        histo[index]->Add((TH1D*)gDirectory->Get(title.c_str()), norm);
+      } else {
+        histo[index] = (TH1D*)gDirectory->Get(title.c_str());
+        histo[index]->SetDirectory(0);
+        histo[index]->Scale(norm);
+      }
+    }
+    file.Close();
+  }
+
+  for (uint i = 0; i < sizeof(histo)/sizeof(histo[0]); i++) {
+    if (histo[i]) {
+      cout << i << " " << histo[i]->Integral() << endl;
+    }
   }
 
 }
