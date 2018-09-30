78ddc90d9d50395c02b76fc0a40b83b64c71551f
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-09-30 11:40:20 +0200

    move maps reading code to helper function

--- VggTools/macros/plot0.C
+++ VggTools/macros/plot0.C
@@ -1,39 +1,15 @@
 #include "plot0.h"
 
+#include "utils.C"
+
 void plot0(string title="") {
 
-  map<string, float> lumiData;
+  map<string, float> lumiMap;
+  readMap("lumi.dat", lumiMap);
+  cout << "Read lumi map for " << lumiMap.size() << " datasets" << endl;
 
-  ifstream input0("lumi.dat");
-  string line0;
-  while (getline(input0, line0)) {
-    if (line0.length() > 0 && line0[0] != '#') {
-      istringstream stream(line0);
-      string key;
-      float val;
-      stream >> key >> val;
-      lumiData[key] = val;
-//      cout << key << " " << val << endl;
-    }
-  }
-  input0.close();
-  cout << "Read lumi data for " << lumiData.size() << " datasets" << endl;
-
-  map<string, float> xsecData;
-
-  ifstream input1("xsec.dat");
-  string line1;
-  while (getline(input0, line0)) {
-    if (line0.length() > 0 && line0[0] != '#') {
-      istringstream stream(line0);
-      string key;
-      float val;
-      stream >> key >> val;
-      xsecData[key] = val;
-//      cout << key << " " << val << endl;
-    }
-  }
-  input1.close();
-  cout << "Read xsec data for " << xsecData.size() << " datasets" << endl;
+  map<string, float> xsecMap;
+  readMap("xsec.dat", xsecMap);
+  cout << "Read xsec map for " << xsecMap.size() << " datasets" << endl;
 
 }
