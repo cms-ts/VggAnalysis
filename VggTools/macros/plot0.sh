888898a4a263bdbf03f7866bc17f5f3e35129f93
Alessandro Da Rold <alessandro.da.rold@cern.ch>
2018-10-04 18:07:19 +0200

    added different primary vertex histo for ele and muons

--- VggTools/macros/plot0.sh
+++ VggTools/macros/plot0.sh
@@ -9,9 +9,9 @@ export ROOT_HIST=0
 root-6.12 -l -q -b plot0.C+\(\"Zgg2016_ele.dat\",\"h_Z_ele\"\)
 root-6.12 -l -q -b plot0.C+\(\"Zgg2016_muo.dat\",\"h_Z_muo\"\)
 
-root-6.12 -l -q -b plot0.C+\(\"Zgg2016_ele.dat\",\"h_npvs\"\)
-#root-6.12 -l -q -b plot0.C+\(\"Zgg2016_muo.dat\",\"h_npvs\"\)
-root-6.12 -l -q -b plot0.C+\(\"Zgg2016_ele.dat\",\"h_npvs_w\"\)
-#root-6.12 -l -q -b plot0.C+\(\"Zgg2016_muo.dat\",\"h_npvs_w\"\)
+root-6.12 -l -q -b plot0.C+\(\"Zgg2016_ele.dat\",\"h_npvs_ele\"\)
+root-6.12 -l -q -b plot0.C+\(\"Zgg2016_muo.dat\",\"h_npvs_muo\"\)
+root-6.12 -l -q -b plot0.C+\(\"Zgg2016_ele.dat\",\"h_npvs_ele_w\"\)
+root-6.12 -l -q -b plot0.C+\(\"Zgg2016_muo.dat\",\"h_npvs_muo_w\"\)
 
 exit
