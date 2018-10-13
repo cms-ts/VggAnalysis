5d34279d6c65aa13bcc950c2ab652db085578729
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-10-13 14:23:01 +0200

    improve logs checks

--- VggTools/macros/check_logs.sh
+++ VggTools/macros/check_logs.sh
@@ -10,24 +10,33 @@ WORKDIR=/home/$USER/work/cms/VggAnalysis/VggTools/macros
 
 cd $WORKDIR
 
-cat data/$VERSION/*.log | \
-grep [a-z,A-Z] | \
-grep -v 'Processing' | \
-grep -v 'Starting PROOF-Lite' | \
-grep -v 'Opening connections to workers:' | \
-grep -v 'Setting up worker servers:' |
-grep -v 'PROOF set to parallel mode' | \
-grep -v 'Info' | \
-grep -v "Looking up for exact location of files:" | \
-grep -v 'Validating files:' | \
-grep -v 'entries' | \
-grep -v 'TProof::Progress' | \
-grep -v 'Query processing time' | \
-grep -v 'Lite-0' | \
-grep -v '+++' | \
-grep -v '(M)' | \
-grep -v '#' | \
-grep -v 'RoccoR::kScaleAndSmearMC' | \
-sed -e 's;'$WORKDIR'/lists/;;'
+for L in `ls data/$VERSION/*.log`; do
+  O=`cat $L | \
+  grep [a-z,A-Z] | \
+  grep -v 'Processing' | \
+  grep -v 'input list' | \
+  grep -v 'Starting PROOF-Lite' | \
+  grep -v 'Opening connections to workers:' | \
+  grep -v 'Setting up worker servers:' |
+  grep -v 'PROOF set to parallel mode' | \
+  grep -v 'Info' | \
+  grep -v "Looking up for exact location of files:" | \
+  grep -v 'Validating files:' | \
+  grep -v 'entries' | \
+  grep -v 'TProof::Progress' | \
+  grep -v 'Query processing time' | \
+  grep -v 'Lite-0' | \
+  grep -v '+++' | \
+  grep -v '(M)' | \
+  grep -v '#' | \
+  grep -v 'RoccoR::kScaleAndSmearMC'`
+  echo -n 'Checking '
+  basename $L
+  if [ -z "$O" ]; then
+    echo "OK"
+  else
+    echo "$O"
+  fi
+done
 
 exit
