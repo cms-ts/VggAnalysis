dcfb9db125a5317ca2a5ac404d52dee9362dd008
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-10-18 16:19:47 +0200

    fix flags to differentiate the MC datasets

--- VggTools/macros/run.C
+++ VggTools/macros/run.C
@@ -69,12 +69,12 @@ void run(TString input="lists/Run2017B_DoubleEG.list", TString output="selector.
     if (option.Contains("MC18")) selector = TSelector::GetSelector("mainSelectorMC18.C+");
 #endif
     selector->SetOption("MC");
-    if (option.Contains("WJetsToLNu")) selector->SetOption("MC,WJetsToLNu");
-    if (option.Contains("DYJetsToLL")) selector->SetOption("MC,DYJetsToLL");
-    if (option.Contains("WGToLNuG")) selector->SetOption("MC,WGToLNuG");
-    if (option.Contains("WGGJets")) selector->SetOption("MC,WGGJets");
-    if (option.Contains("ZGTo2LG")) selector->SetOption("MC,ZGTo2LG");
-    if (option.Contains("ZGGJetsToLLGG")) selector->SetOption("MC,ZGGJetsToLLGG");
+    if (input.Contains("WJetsToLNu")) selector->SetOption("MC,WJetsToLNu");
+    if (input.Contains("DYJetsToLL")) selector->SetOption("MC,DYJetsToLL");
+    if (input.Contains("WGToLNuG")) selector->SetOption("MC,WGToLNuG");
+    if (input.Contains("WGGJets")) selector->SetOption("MC,WGGJets");
+    if (input.Contains("ZGTo2LG")) selector->SetOption("MC,ZGTo2LG");
+    if (input.Contains("ZGGJetsToLLGG")) selector->SetOption("MC,ZGGJetsToLLGG");
     workers.Process(files, *selector, "Events", nevt);
   }
 
