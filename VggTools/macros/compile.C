28e2e037d786805e2dfb3f4f52a9a56e224a8f58
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-10-29 13:58:14 +0100

    cosmetics

--- VggTools/macros/compile.C
+++ VggTools/macros/compile.C
@@ -1,12 +1,13 @@
 void compile(string option="kcO") {
 
   gSystem->CompileMacro("mainSelectorDT16.C",  option.c_str());
+  gSystem->CompileMacro("mainSelectorMC16.C",  option.c_str());
+
   gSystem->CompileMacro("mainSelectorDT17.C",  option.c_str());
   gSystem->CompileMacro("mainSelectorDT17B.C", option.c_str());
-  gSystem->CompileMacro("mainSelectorDT18.C",  option.c_str());
-
-  gSystem->CompileMacro("mainSelectorMC16.C",  option.c_str());
   gSystem->CompileMacro("mainSelectorMC17.C",  option.c_str());
+
+  gSystem->CompileMacro("mainSelectorDT18.C",  option.c_str());
   gSystem->CompileMacro("mainSelectorMC18.C",  option.c_str());
 
   gSystem->CompileMacro("plot0.C", option.c_str());
