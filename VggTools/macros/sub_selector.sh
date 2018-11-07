ebac9e6d063782bfcd02d0d86d4ce88c7c8019af
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-11-07 14:24:41 +0100

    add attempt to skip overloaded CPUs

--- VggTools/macros/sub_selector.sh
+++ VggTools/macros/sub_selector.sh
@@ -3,6 +3,13 @@
 QUEUE=normal
 export USE_LSF_STARTER=no
 
+EXCLUDED_HOSTS=""
+EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm003 && hname!=farm004 && hname!=farm005 && hname!=farm006 && "
+EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm019 && hname!=farm020 && hname!=farm021 && hname!=farm022 && "
+EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm025 && hname!=farm026 && "
+EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm073 && hname!=farm074 && hname!=farm075 && hname!=farm076 && "
+EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm061 && hname!=farm062 && hname!=farm063 && hname!=farm064"
+
 VERSION=v00
 
 if [ ! -z "$1" ]; then
@@ -38,7 +45,7 @@ fi
 ./compile.sh
 
 for L in $LISTS; do
-  bsub -q $QUEUE -J $L -e /dev/null -o /dev/null $WORKDIR/job_selector.sh $VERSION lists/$L
+  bsub -q $QUEUE -R "$EXCLUDED_HOSTS" -J $L -e /dev/null -o /dev/null $WORKDIR/job_selector.sh $VERSION lists/$L
 done
 
 exit
