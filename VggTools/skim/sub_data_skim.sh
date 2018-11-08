652d70bfe673f5a134509f2173d7c32c9af97f57
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-11-08 13:20:28 +0100

    add attempt to skip overloaded CPUs (skimming jobs)

--- VggTools/skim/sub_data_skim.sh
+++ VggTools/skim/sub_data_skim.sh
@@ -3,6 +3,13 @@
 QUEUE=normal_io
 export USE_LSF_STARTER=no
 
+EXCLUDED_HOSTS=""
+EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm003 && hname!=farm004 && hname!=farm005 && hname!=farm006 && "
+EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm019 && hname!=farm020 && hname!=farm021 && hname!=farm022 && "
+EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm025 && hname!=farm026 && "
+EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm073 && hname!=farm074 && hname!=farm075 && hname!=farm076 && "
+EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm061 && hname!=farm062 && hname!=farm063 && hname!=farm064"
+
 DATADIR=/eos/infnts/cms/store/data
 WORKDIR=/home/$USER/work/cms/VggAnalysis/VggTools/skim
 
@@ -12,7 +19,7 @@ for F in `eos find -f $DATADIR/ | grep Run2016 | grep NANOAOD | grep 05Feb2018`;
   if [ -s $F ]; then
     if [ ! -e logs/`basename $F .root`_Skim.log ]; then
       eos mkdir -p `dirname $F | sed -e 's;data;user/dellaric/data;'`
-      bsub -q $QUEUE -e /dev/null -o /dev/null job_data_skim.sh $F
+      bsub -q $QUEUE -R "$EXCLUDED_HOSTS" -e /dev/null -o /dev/null job_data_skim.sh $F
     fi
   fi
 done
@@ -21,7 +28,7 @@ for F in `eos find -f $DATADIR/ | grep Run2016 | grep NANOAOD | grep 22Aug2018`;
   if [ -s $F ]; then
     if [ ! -e logs/`basename $F .root`_Skim.log ]; then
       eos mkdir -p `dirname $F | sed -e 's;data;user/dellaric/data;'`
-      bsub -q $QUEUE -e /dev/null -o /dev/null job_data_skim.sh $F
+      bsub -q $QUEUE -R "$EXCLUDED_HOSTS" -e /dev/null -o /dev/null job_data_skim.sh $F
     fi
   fi
 done
@@ -30,7 +37,7 @@ for F in `eos find -f $DATADIR/ | grep Run2017 | grep NANOAOD | grep 31Mar2018`;
   if [ -s $F ]; then
     if [ ! -e logs/`basename $F .root`_Skim.log ]; then
       eos mkdir -p `dirname $F | sed -e 's;data;user/dellaric/data;'`
-      bsub -q $QUEUE -e /dev/null -o /dev/null job_data_skim.sh $F
+      bsub -q $QUEUE -R "$EXCLUDED_HOSTS" -e /dev/null -o /dev/null job_data_skim.sh $F
     fi
   fi
 done
@@ -39,7 +46,7 @@ for F in `eos find -f $DATADIR/ | grep Run2018 | grep NANOAOD | grep 14Sep2018`;
   if [ -s $F ]; then
     if [ ! -e logs/`basename $F .root`_Skim.log ]; then
       eos mkdir -p `dirname $F | sed -e 's;data;user/dellaric/data;'`
-      bsub -q $QUEUE -e /dev/null -o /dev/null job_data_skim.sh $F
+      bsub -q $QUEUE -R "$EXCLUDED_HOSTS" -e /dev/null -o /dev/null job_data_skim.sh $F
     fi
   fi
 done
