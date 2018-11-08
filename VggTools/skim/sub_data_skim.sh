f6fd9aa259591e648dbfa604ad4d92d88f5742ce
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-11-08 20:46:00 +0100

    skip two more 'acid' hosts

--- VggTools/skim/sub_data_skim.sh
+++ VggTools/skim/sub_data_skim.sh
@@ -7,6 +7,7 @@ EXCLUDED_HOSTS=""
 EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm003 && hname!=farm004 && hname!=farm005 && hname!=farm006 && "
 EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm019 && hname!=farm020 && hname!=farm021 && hname!=farm022 && "
 EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm025 && hname!=farm026 && "
+EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm045 && hname!=farm046 && "
 EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm073 && hname!=farm074 && hname!=farm075 && hname!=farm076 && "
 EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm061 && hname!=farm062 && hname!=farm063 && hname!=farm064"
 
