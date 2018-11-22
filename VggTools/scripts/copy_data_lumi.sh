77b5615e94e2b824f363a576bc2881f736909f9b
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-11-22 18:49:58 +0100

    fix output file layout

--- VggTools/scripts/copy_data_lumi.sh
+++ VggTools/scripts/copy_data_lumi.sh
@@ -9,30 +9,30 @@ rm -f ../macros/lumi.dat
 for F in `ls logs/*.log`; do
   G=`basename $F .log`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
-  [ -z "${F##*DoubleEG_22Aug2018*}"       ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
-  [ -z "${F##*DoubleMuon_22Aug2018*}"     ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
-  [ -z "${F##*SingleElectron_22Aug2018*}" ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
-  [ -z "${F##*SingleMuon_22Aug2018*}"     ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
-  [ -z "${F##*MuonEG_22Aug2018*}"         ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*DoubleEG_22Aug2018*}"       ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*DoubleMuon_22Aug2018*}"     ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*SingleElectron_22Aug2018*}" ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*SingleMuon_22Aug2018*}"     ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*MuonEG_22Aug2018*}"         ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
 done
 
 for F in `ls logs/*.log`; do
   G=`basename $F .log`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
-  [ -z "${F##*DoubleEG_31Mar2018*}"       ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
-  [ -z "${F##*DoubleMuon_31Mar2018*}"     ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
-  [ -z "${F##*SingleElectron_31Mar2018*}" ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
-  [ -z "${F##*SingleMuon_31Mar2018*}"     ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
-  [ -z "${F##*MuonEG_31Mar2018*}"         ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*DoubleEG_31Mar2018*}"       ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*DoubleMuon_31Mar2018*}"     ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*SingleElectron_31Mar2018*}" ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*SingleMuon_31Mar2018*}"     ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*MuonEG_31Mar2018*}"         ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
 done
 
 for F in `ls logs/*.log`; do
   G=`basename $F .log`
   L=`grep -A 5 Summary $F | grep [0-9] | awk '{print $12}'`
-  [ -z "${F##*EGamma_14Sep2018*}"         ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
-  [ -z "${F##*DoubleMuon_14Sep2018*}"     ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
-  [ -z "${F##*SingleMuon_14Sep2018*}"     ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
-  [ -z "${F##*MuonEG_14Sep2018*}"         ] && printf "%s\t%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*EGamma_14Sep2018*}"         ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*DoubleMuon_14Sep2018*}"     ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*SingleMuon_14Sep2018*}"     ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
+  [ -z "${F##*MuonEG_14Sep2018*}"         ] && printf "%-40s%6.3f\n" $G $L >> ../macros/lumi.dat
 done
 
 exit
