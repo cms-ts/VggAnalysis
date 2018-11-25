689df6e2ece895ee95d59acbf5c91077a8d68c6b
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-11-25 09:39:22 +0100

    rename scritps/pileup/ in scripts/root/ for consistency with macros/root

--- VggTools/scripts/create_mc_pileup_2016.py
+++ VggTools/scripts/create_mc_pileup_2016.py
@@ -16,7 +16,7 @@ hist = ROOT.TH1D("pileup", "pileup", binMax-binMin, binMin, binMax)
 for bin in mix.input.nbPileupEvents.probFunctionVariable:
     hist.SetBinContent(bin+1, mix.input.nbPileupEvents.probValue[bin])
 
-file = ROOT.TFile.Open("pileup/pileup_mc_2016.root", "recreate")
+file = ROOT.TFile.Open("root/pileup_mc_2016.root", "recreate")
 
 hist.Write()
 file.Write()
