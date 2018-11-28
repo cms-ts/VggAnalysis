423ee11ffadc8d2c4b46a8cd27fbab1a87ac6c59
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-11-28 13:06:38 +0100

    simplify logic (also) at gen level [iele1 == -1 implies iele0 == -1, imuo1 == -1 implies imuo0 == -1]

--- VggTools/macros/mainSelector.C
+++ VggTools/macros/mainSelector.C
@@ -641,7 +641,7 @@ Bool_t mainSelector::Process(Long64_t entry)
 
      float W_ele0_gen_mt = 0.;
 
-     if (iele0_gen != -1 && iele1_gen == -1 && imuo0_gen == -1 && imuo1_gen == -1) {
+     if (iele0_gen != -1 && iele1_gen == -1 && imuo1_gen == -1) {
        W_ele0_gen_mt = TMath::Sqrt(2. * ele0_gen.Pt() * (*MET_fiducialGenPt) * (1. - TMath::Cos(ele0_gen.Phi() - (*MET_fiducialGenPhi))));
        if (*MET_fiducialGenPt > 0 && W_ele0_gen_mt > 40 && ele0_gen.Pt() > 40) {
          W_ele_sel_gen = true;
@@ -652,7 +652,7 @@ Bool_t mainSelector::Process(Long64_t entry)
 
      float W_muo0_gen_mt = 0.;
 
-     if (imuo0_gen != -1 && imuo1_gen == -1 && iele0_gen == -1 && iele1_gen == -1) {
+     if (imuo0_gen != -1 && imuo1_gen == -1 && iele1_gen == -1) {
        W_muo0_gen_mt = TMath::Sqrt(2. * muo0_gen.Pt() * (*MET_fiducialGenPt) * (1. - TMath::Cos(muo0_gen.Phi() - (*MET_fiducialGenPhi))));
        if (*MET_fiducialGenPt > 0 && W_muo0_gen_mt > 40 && muo0_gen.Pt() > 30) {
          W_muo_sel_gen = true;
