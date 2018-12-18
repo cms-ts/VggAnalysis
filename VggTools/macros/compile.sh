186d0ab02d2015de1c13a9b221af4b3728cce721
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-12-18 07:17:36 +0100

    use same ROOT version everywhere

--- VggTools/macros/compile.sh
+++ VggTools/macros/compile.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+if [ -r $CMS_PATH/slc6_amd64_gcc700/cms/cmssw/CMSSW_10_3_2 ]; then
+  cd $CMS_PATH/slc6_amd64_gcc700/cms/cmssw/CMSSW_10_3_2
+  eval `scramv1 runtime -sh`
+  cd $OLDPWD
+  alias root-6.12=root
+fi
+
+WORKDIR=/home/$USER/work/cms/VggAnalysis/VggTools/macros
+
+cd $WORKDIR
+
 export ROOT_HIST=0
 
 if [ -z "$1" ]; then
