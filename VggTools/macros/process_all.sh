fe2f07f2154cfc376847022efae5a239d54da647
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-12-19 07:13:22 +0100

    save example

--- VggTools/macros/process_all.sh
+++ VggTools/macros/process_all.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 WORKDIR=/home/$USER/work/cms/VggAnalysis/VggTools/macros
+
 cd $WORKDIR
 
-./process.sh v06 all
+./process.sh v06 all "nolog,amcatnlo nolog,madgraph"
+
+./process.sh v06 all "nolog,amcatnlo,test nolog,madgraph,test"
 
 exit
