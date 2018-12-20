ea8cd093064af18d7e7ad0cf17ae157cb1484a63
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-12-20 08:27:28 +0100

    update process_all

--- VggTools/macros/process_all.sh
+++ VggTools/macros/process_all.sh
@@ -4,8 +4,12 @@ WORKDIR=/home/$USER/work/cms/VggAnalysis/VggTools/macros
 
 cd $WORKDIR
 
-./process.sh v06 all "nolog,amcatnlo nolog,madgraph"
+if [ ! -z "$1" ]; then
+  VERSION=$1
+fi
 
-./process.sh v06 all "nolog,amcatnlo,test nolog,madgraph,test"
+./process.sh $VERSION all "nolog,amcatnlo nolog,madgraph"
+
+./process.sh $VERSION all "nolog,amcatnlo,test nolog,madgraph,test"
 
 exit
