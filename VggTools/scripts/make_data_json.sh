b7fe3ab99483a64fb17a31eccf56ad0e8a3e35b9
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-12-21 11:28:40 +0100

    cleanup code by reusing .list files

--- VggTools/scripts/make_data_json.sh
+++ VggTools/scripts/make_data_json.sh
@@ -9,52 +9,22 @@ DATADIR=/eos/infnts/cms/store/user/dellaric/data
 WORKDIR=/home/$USER/work/cms/VggAnalysis/VggTools/scripts
 cd $WORKDIR
 
-for D1 in `ls $DATADIR/ | grep Run2016`; do
-  for D2 in `ls $DATADIR/$D1`; do
-    N=`find $DATADIR/$D1/$D2 -type f | grep 14Dec2018 | wc -l`
-    echo "checking ${D1}/${D2} - $N files"
-    python nano_report.py -o json/${D1}_${D2}_14Dec2018.json \
-    `find $DATADIR/$D1/$D2 -type f | grep 14Dec2018`
-  done
+for F in `ls lists/Run2016*.list`; do
+  echo "checking $F - "`wc -l $F`" files"
+  python nano_report.py -o json/`basename $F .list`.json \
+  `cat $F | sed -e 's;root://eosinfnts.ts.infn.it/;;'`
 done
 
-for D1 in `ls $DATADIR/ | grep Run2017`; do
-  for D2 in `ls $DATADIR/$D1`; do
-    N=`find $DATADIR/$D1/$D2 -type f | grep 14Dec2018 | wc -l`
-    echo "checking ${D1}/${D2} - $N files"
-    python nano_report.py -o json/${D1}_${D2}_14Dec2018.json \
-    `find $DATADIR/$D1/$D2 -type f | grep 14Dec2018`
-  done
+for F in `ls lists/Run2017*.list`; do
+  echo "checking $F - "`wc -l $F`" files"
+  python nano_report.py -o json/`basename $F .list`.json \
+  `cat $F | sed -e 's;root://eosinfnts.ts.infn.it/;;'`
 done
 
-for D1 in `ls $DATADIR/ | grep Run2018`; do
-  for D2 in `ls $DATADIR/$D1`; do
-    N=`find $DATADIR/$D1/$D2 -type f | grep 14Dec2018 | wc -l`
-    echo "checking ${D1}/${D2} - $N files"
-    python nano_report.py -o json/${D1}_${D2}_14Dec2018.json \
-    `find $DATADIR/$D1/$D2 -type f | grep 14Dec2018`
-  done
-done
-
-for D2 in `ls $DATADIR/Run2016B/`; do
-  N=`find $DATADIR/Run2016*/$D2 -type f | grep 14Dec2018 | wc -l`
-  echo "checking Run2016/${D2} - $N files"
-  python nano_report.py -o json/Run2016_${D2}_14Dec2018.json \
-  `find $DATADIR/Run2016*/$D2 -type f | grep 14Dec2018`
-done
-
-for D2 in `ls $DATADIR/Run2017B/`; do
-  N=`find $DATADIR/Run2017*/$D2 -type f | grep 14Dec2018 | wc -l`
-  echo "checking Run2017/${D2} - $N files"
-  python nano_report.py -o json/Run2017_${D2}_14Dec2018.json \
-  `find $DATADIR/Run2017*/$D2 -type f | grep 14Dec2018`
-done
-
-for D2 in `ls $DATADIR/Run2018B/`; do
-  N=`find $DATADIR/Run2018*/$D2 -type f | grep 14Dec2018 | wc -l`
-  echo "checking Run2018/${D2} - $N files"
-  python nano_report.py -o json/Run2018_${D2}_14Dec2018.json \
-  `find $DATADIR/Run2018*/$D2 -type f | grep 14Dec2018`
+for F in `ls lists/Run2018*.list`; do
+  echo "checking $F - "`wc -l $F`" files"
+  python nano_report.py -o json/`basename $F .list`.json \
+  `cat $F | sed -e 's;root://eosinfnts.ts.infn.it/;;'`
 done
 
 exit
