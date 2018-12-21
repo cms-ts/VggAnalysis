c1ce880de9b35c5c1aba51bcc7f2d83262efab1d
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-12-21 12:54:46 +0100

    do not limit copies to only one processing

--- VggTools/scripts/copy_pileup.sh
+++ VggTools/scripts/copy_pileup.sh
@@ -3,7 +3,7 @@
 WORKDIR=/home/$USER/work/cms/VggAnalysis/VggTools/scripts
 cd $WORKDIR
 
-for F in `ls root/ratio_pileup_Run2016_* | grep 14Dec2018`; do
+for F in `ls root/ratio_pileup_Run2016_*`; do
   [ -z "${F##*DoubleEG*}"       ] && cp -p $F ../macros/root/
   [ -z "${F##*DoubleMuon*}"     ] && cp -p $F ../macros/root/
   [ -z "${F##*SingleElectron*}" ] && cp -p $F ../macros/root/
@@ -11,7 +11,7 @@ for F in `ls root/ratio_pileup_Run2016_* | grep 14Dec2018`; do
   [ -z "${F##*MuonEG*}"         ] && cp -p $F ../macros/root/
 done
 
-for F in `ls root/ratio_pileup_Run2017_* | grep 14Dec2018`; do
+for F in `ls root/ratio_pileup_Run2017_*`; do
   [ -z "${F##*DoubleEG*}"       ] && cp -p $F ../macros/root/
   [ -z "${F##*DoubleMuon*}"     ] && cp -p $F ../macros/root/
   [ -z "${F##*SingleElectron*}" ] && cp -p $F ../macros/root/
@@ -19,7 +19,7 @@ for F in `ls root/ratio_pileup_Run2017_* | grep 14Dec2018`; do
   [ -z "${F##*MuonEG*}"         ] && cp -p $F ../macros/root/
 done
 
-for F in `ls root/ratio_pileup_Run2018_* | grep 14Dec2018`; do
+for F in `ls root/ratio_pileup_Run2018_*`; do
   [ -z "${F##*EGamma*}"         ] && cp -p $F ../macros/root/
   [ -z "${F##*DoubleMuon*}"     ] && cp -p $F ../macros/root/
   [ -z "${F##*SingleMuon*}"     ] && cp -p $F ../macros/root/
