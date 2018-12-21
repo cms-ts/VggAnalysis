f7089e233634979cc5d9c6e3ad3720af8c2ed277
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2018-12-21 12:00:36 +0100

    fix previous commit (again)

--- VggTools/scripts/copy_lists.sh
+++ VggTools/scripts/copy_lists.sh
@@ -4,33 +4,33 @@ WORKDIR=/home/$USER/work/cms/VggAnalysis/VggTools/scripts
 cd $WORKDIR
 
 for F in `ls lists/Run2016[A-Z]*.list`; do
-  [ -z "${F##*DoubleEG*}"       ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*DoubleMuon*}"     ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*SingleElectron*}" ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*SingleMuon*}"     ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*MuonEG*}"         ] && cp -p lists/$L ../macros/lists/
+  [ -z "${F##*DoubleEG*}"       ] && cp -p $F ../macros/lists/
+  [ -z "${F##*DoubleMuon*}"     ] && cp -p $F ../macros/lists/
+  [ -z "${F##*SingleElectron*}" ] && cp -p $F ../macros/lists/
+  [ -z "${F##*SingleMuon*}"     ] && cp -p $F ../macros/lists/
+  [ -z "${F##*MuonEG*}"         ] && cp -p $F ../macros/lists/
 done
 
 for F in `ls lists/Run2017[A-Z]*.list`; do
-  [ -z "${F##*DoubleEG*}"       ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*DoubleMuon*}"     ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*SingleElectron*}" ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*SingleMuon*}"     ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*MuonEG*}"         ] && cp -p lists/$L ../macros/lists/
+  [ -z "${F##*DoubleEG*}"       ] && cp -p $F ../macros/lists/
+  [ -z "${F##*DoubleMuon*}"     ] && cp -p $F ../macros/lists/
+  [ -z "${F##*SingleElectron*}" ] && cp -p $F ../macros/lists/
+  [ -z "${F##*SingleMuon*}"     ] && cp -p $F ../macros/lists/
+  [ -z "${F##*MuonEG*}"         ] && cp -p $F ../macros/lists/
 done
 
 for F in `ls lists/Run2018[A-Z]*.list`; do
-  [ -z "${F##*EGamma*}"         ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*DoubleMuon*}"     ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*SingleMuon*}"     ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*MuonEG*}"         ] && cp -p lists/$L ../macros/lists/
+  [ -z "${F##*EGamma*}"         ] && cp -p $F ../macros/lists/
+  [ -z "${F##*DoubleMuon*}"     ] && cp -p $F ../macros/lists/
+  [ -z "${F##*SingleMuon*}"     ] && cp -p $F ../macros/lists/
+  [ -z "${F##*MuonEG*}"         ] && cp -p $F ../macros/lists/
 done
 
-for L in `ls lists/RunII*.list`; do
-  [ -z "${F##*RunIISummer16*}"  ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*RunIIFall17*}"    ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*RunIIFall18*}"    ] && cp -p lists/$L ../macros/lists/
-  [ -z "${F##*RunIIAutumn18*}"  ] && cp -p lists/$L ../macros/lists/
+for F in `ls lists/RunII*.list`; do
+  [ -z "${F##*RunIISummer16*}"  ] && cp -p $F ../macros/lists/
+  [ -z "${F##*RunIIFall17*}"    ] && cp -p $F ../macros/lists/
+  [ -z "${F##*RunIIFall18*}"    ] && cp -p $F ../macros/lists/
+  [ -z "${F##*RunIIAutumn18*}"  ] && cp -p $F ../macros/lists/
 done
 
 exit
