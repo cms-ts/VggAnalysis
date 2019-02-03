63ed6e6fe7fe9622b217db9f90cc86db1f736f8e
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2019-02-03 21:46:12 +0100

    fix conditions for submitting pileup_ele and pileup_muo jobs

--- VggTools/macros/sub_auto_pu2017.sh
+++ VggTools/macros/sub_auto_pu2017.sh
@@ -28,12 +28,12 @@ for L in $LISTS; do
     continue
   fi
   FILE_ELE=data/auto_pu2017/pileup_ele_`basename $L .list`.root
-  if [ ! -e $FILE_ELE ] || [ lists/$L -ot $FILE_ELE ]; then
+  if [ lists/$L -nt $FILE_ELE ]; then
     bsub -q $QUEUE -R "$EXCLUDED_HOSTS" -J $L -e /dev/null -o /dev/null $WORKDIR/job_auto_pu2017.sh lists/$L $FILE_ELE
     check=1
   fi
   FILE_MUO=data/auto_pu2017/pileup_muo_`basename $L .list`.root
-  if [ ! -e $FILE_MUO ] || [ $L -ot $FILE_MUO ]; then
+  if [ $L -nt $FILE_MUO ]; then
     bsub -q $QUEUE -R "$EXCLUDED_HOSTS" -J $L -e /dev/null -o /dev/null $WORKDIR/job_auto_pu2017.sh lists/$L $FILE_MUO
     check=1
   fi
