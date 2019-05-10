fdf945fbfdf2b1a7ffb96e4efb25ed95b057ccf6
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2019-05-10 11:52:44 +0200

    add (empty) plot6 skeleton

---
+++ VggTools/macros/plot6.C
@@ -0,0 +1,82 @@
+#include "plot6.h"
+
+#include "CMS_lumi.C"
+
+void plot6(string plot="", string title="", string version="v00", string options="", string flags="reference") {
+
+  string year = "";
+
+  if (plot.find("2016") != string::npos) year = "2016";
+  if (plot.find("2017") != string::npos) year = "2017";
+  if (plot.find("2018") != string::npos) year = "2018";
+  if (plot.find("Run2") != string::npos) year = "Run2";
+
+  plot = plot + ".dat";
+  if (options.find("test") != string::npos) plot = plot + ".test";
+  if (options.find("new") != string::npos) plot = plot + ".new";
+  if (options.find("jet") != string::npos) plot = plot + ".jet";
+
+  if (options.find("amcatnlo") != string::npos) plot = "amcatnlo/" + plot;
+  if (options.find("madgraph") != string::npos) plot = "madgraph/" + plot;
+  if (options.find("default") != string::npos) plot = "default/" + plot;
+
+  map<string, float> lumiMap;
+  readMap("lumi.dat", lumiMap);
+  cout << "Read lumi map for " << lumiMap.size() << " datasets from " << "lumi.dat" << endl;
+
+  multimap<string, float> plotMap;
+  if (plot.find("Run2") == string::npos) {
+    readMultiMap(plot, plotMap);
+  } else {
+    readMultiMap(string(plot).replace(plot.find("Run2"), 4, "2016"), plotMap);
+    readMultiMap(string(plot).replace(plot.find("Run2"), 4, "2017"), plotMap);
+    readMultiMap(string(plot).replace(plot.find("Run2"), 4, "2018"), plotMap);
+  }
+  cout << "Read plot map for " << plotMap.size() << " datasets from " << plot << endl;
+
+  if (plotMap.size() == 0) {
+    cout << "ERROR: plot map " << plot << " is EMPTY or MISSING !!" << endl;
+    return;
+  }
+
+  float lumi = 0.;
+  float lumi2016 = 0.;
+  float lumi2017 = 0.;
+  float lumi2018 = 0.;
+
+  for (multimap<string, float>::iterator it = plotMap.begin(); it != plotMap.end(); it++) {
+    int index = int(it->second);
+    if (index == 0) {
+      if (lumiMap[it->first] != 0) {
+        lumi = lumi + lumiMap[it->first];
+        lumi2016 = lumi2016 + lumiMap[it->first];
+        lumi2017 = lumi2017 + lumiMap[it->first];
+        lumi2018 = lumi2018 + lumiMap[it->first];
+      } else {
+        cout << "WARNING: luminosity for " << it->first << " is ZERO !!" << endl;
+      }
+    }
+  }
+
+  if (lumi == 0) {
+    cout << "ERROR: total luminosity is ZERO !!" << endl;
+    return;
+  }
+
+  if (options.find("test") != string::npos) version = version + ".test";
+  if (options.find("new") != string::npos) version = version + ".new";
+  if (options.find("jet") != string::npos) version = version + ".jet";
+
+  if (options.find("amcatnlo") != string::npos) version = version + ".amcatnlo";
+  if (options.find("madgraph") != string::npos) version = version + ".madgraph";
+  if (options.find("default") != string::npos) version = version + ".default";
+
+// TODO
+
+}
+
+int main(int argc, char *argv[]) {
+
+plot6(argv[1], argv[2], argv[3], argv[4]);
+
+}
---
+++ VggTools/macros/plot6.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+if [ -r $CMS_PATH/slc6_amd64_gcc700/cms/cmssw/CMSSW_10_3_2 ]; then
+  cd $CMS_PATH/slc6_amd64_gcc700/cms/cmssw/CMSSW_10_3_2
+  eval `scramv1 runtime -sh`
+  cd $OLDPWD
+  alias root-6.12=root
+fi
+
+WORKDIR=/home/$USER/work/cms/VggAnalysis/VggTools/macros
+cd $WORKDIR
+
+VERSION="v10"
+
+if [ ! -z "$1" ]; then
+  VERSION=$1
+fi
+
+YEARS="2016"
+
+if [ ! -z "$2" ]; then
+  if [ "$2" == "all" ]; then
+    YEARS="2016 2017 2018"
+  else
+    YEARS=$2
+  fi
+fi
+
+OPTIONS="nolog,amcatnlo"
+
+if [ ! -z "$3" ]; then
+  OPTIONS=$3
+fi
+
+FLAGS=""
+
+if [ ! -z "$4" ]; then
+  FLAGS=$4
+fi
+
+export ROOT_HIST=0
+
+for YEAR in $YEARS; do
+
+  root-6.12 -l -q -b plot6.C+\(\"Wgg"$YEAR"_ele\",\"h_W_ele_nphotons\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+  root-6.12 -l -q -b plot6.C+\(\"Wgg"$YEAR"_muo\",\"h_W_muo_nphotons\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+
+  root-6.12 -l -q -b plot6.C+\(\"Zgg"$YEAR"_ele\",\"h_Z_ele_nphotons\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+  root-6.12 -l -q -b plot6.C+\(\"Zgg"$YEAR"_muo\",\"h_Z_muo_nphotons\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+
+  root-6.12 -l -q -b plot6.C+\(\"Wgg"$YEAR"_ele\",\"h_WGG_ele_pho0_pt\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+  root-6.12 -l -q -b plot6.C+\(\"Wgg"$YEAR"_ele\",\"h_WGG_ele_pho1_pt\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+  root-6.12 -l -q -b plot6.C+\(\"Wgg"$YEAR"_ele\",\"h_WGG_ele_pho0_pho1_pt\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+  root-6.12 -l -q -b plot6.C+\(\"Wgg"$YEAR"_muo\",\"h_WGG_muo_pho0_pt\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+  root-6.12 -l -q -b plot6.C+\(\"Wgg"$YEAR"_muo\",\"h_WGG_muo_pho1_pt\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+  root-6.12 -l -q -b plot6.C+\(\"Wgg"$YEAR"_muo\",\"h_WGG_muo_pho0_pho1_pt\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+
+  root-6.12 -l -q -b plot6.C+\(\"Zgg"$YEAR"_ele\",\"h_ZGG_ele_pho0_pt\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+  root-6.12 -l -q -b plot6.C+\(\"Zgg"$YEAR"_ele\",\"h_ZGG_ele_pho1_pt\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+  root-6.12 -l -q -b plot6.C+\(\"Zgg"$YEAR"_ele\",\"h_ZGG_ele_pho0_pho1_pt\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+  root-6.12 -l -q -b plot6.C+\(\"Zgg"$YEAR"_muo\",\"h_ZGG_muo_pho0_pt\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+  root-6.12 -l -q -b plot6.C+\(\"Zgg"$YEAR"_muo\",\"h_ZGG_muo_pho1_pt\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+  root-6.12 -l -q -b plot6.C+\(\"Zgg"$YEAR"_muo\",\"h_ZGG_muo_pho0_pho1_pt\",\"$VERSION\",\"$OPTIONS\",\"$FLAGS\"\)
+
+done
+
+exit
