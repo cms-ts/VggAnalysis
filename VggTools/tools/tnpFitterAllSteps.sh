fb259e07fbdfb5da5ccc3a02ad52718f2ca5cfd2
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2019-05-28 18:48:06 +0200

    add option for different years & fix path

--- VggTools/tools/tnpFitterAllSteps.sh
+++ VggTools/tools/tnpFitterAllSteps.sh
@@ -10,17 +10,24 @@ cd $WORKDIR
 
 PYTHONPATH=$PYTHONPATH:$WORKDIR
 
+YEAR="2016"
+
+if [ ! -z "$1" ]; then
+  YEAR=$1
+fi
+
 FLAGS="passingMedium94X HLTpath"
 
 for F in $FLAGS; do
-    echo $F
-    python /home/darold/work/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_2016.py --flag $F --createBins
-    python /home/darold/work/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_2016.py --flag $F --createHists
-    python /home/darold/work/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_2016.py --flag $F --doFit
-    python /home/darold/work/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_2016.py --flag $F --doFit --mcSig --altSig
-    python /home/darold/work/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_2016.py --flag $F --doFit --altSig
-    python /home/darold/work/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_2016.py --flag $F --doFit --altBkg
-    python /home/darold/work/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_2016.py --flag $F --sumUp
+
+  python $HOME/work/cms/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_$YEAR.py --flag $F --createBins
+  python $HOME/work/cms/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_$YEAR.py --flag $F --createHists
+  python $HOME/work/cms/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_$YEAR.py --flag $F --doFit
+  python $HOME/work/cms/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_$YEAR.py --flag $F --doFit --mcSig --altSig
+  python $HOME/work/cms/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_$YEAR.py --flag $F --doFit --altSig
+  python $HOME/work/cms/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_$YEAR.py --flag $F --doFit --altBkg
+  python $HOME/work/cms/CMSSW_9_4_0/src/egm_tnp_analysis/tnpEGM_fitter.py  settings_ele_$YEAR.py --flag $F --sumUp
+
 done
 
 exit
