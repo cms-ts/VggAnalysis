240b93e2abf55e37531aa1670ba289a9e991543c
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2019-07-21 00:11:30 +0200

    update CMSSW version

--- VggTools/macros/compile.sh
+++ VggTools/macros/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ -r $CMS_PATH/slc6_amd64_gcc700/cms/cmssw/CMSSW_10_3_2 ]; then
-  cd $CMS_PATH/slc6_amd64_gcc700/cms/cmssw/CMSSW_10_3_2
+if [ -r $CMS_PATH/slc6_amd64_gcc700/cms/cmssw/CMSSW_10_5_0 ]; then
+  cd $CMS_PATH/slc6_amd64_gcc700/cms/cmssw/CMSSW_10_5_0
   eval `scramv1 runtime -sh`
   cd $OLDPWD
   alias root-6.12=root
