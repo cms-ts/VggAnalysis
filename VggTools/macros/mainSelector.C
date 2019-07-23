aa90677b66c927ef18389d4b1d38c84408d8bdb7
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2019-07-23 08:10:12 +0200

    update TrigObj_filterBits names for AODv4 & AODv5

--- VggTools/macros/mainSelector.C
+++ VggTools/macros/mainSelector.C
@@ -3354,7 +3354,7 @@ Bool_t mainSelector::Process(Long64_t entry)
          for (uint j = 0; j < 32; j++) {
            if ((TrigObj_filterBits[i] & BIT(j)) == BIT(j)) h_W_ele0_trig->Fill(j+0.5, weight_W_ele);
          }
-         if ((TrigObj_filterBits[i] & 2) == 2) match0 = true; // 2 = WPTight or 1e
+         if ((TrigObj_filterBits[i] & 2) == 2) match0 = true; // 2 = 1e
        }
        if (ele0_qcd.DeltaR(tmp_trg) < 0.3) {
          for (uint j = 0; j < 32; j++) {
@@ -3387,7 +3387,7 @@ Bool_t mainSelector::Process(Long64_t entry)
            if ((TrigObj_filterBits[i] & BIT(j)) == BIT(j)) h_W_muo0_trig->Fill(j+0.5, weight_W_muo);
          }
          if ((TrigObj_filterBits[i] & 2) == 2) match0 = true; // 2 = Iso
-         if ((TrigObj_filterBits[i] & 8) == 8) match0 = true; // 8 = IsoTkMu or 1mu
+         if ((TrigObj_filterBits[i] & 8) == 8) match0 = true; // 8 = 1mu
        }
        if (muo0_qcd.DeltaR(tmp_trg) < 0.1) {
          for (uint j = 0; j < 32; j++) {
@@ -3425,7 +3425,7 @@ Bool_t mainSelector::Process(Long64_t entry)
          for (uint j = 0; j < 32; j++) {
            if ((TrigObj_filterBits[i] & BIT(j)) == BIT(j)) h_Z_ele0_trig->Fill(j+0.5, weight_Z_ele);
          }
-         if ((TrigObj_filterBits[i] & 2) == 2) match0 = true; // 2 = WPTight or 1e
+         if ((TrigObj_filterBits[i] & 2) == 2) match0 = true; // 2 = 1e
        }
        if (ele0_qcd.DeltaR(tmp_trg) < 0.3) {
          for (uint j = 0; j < 32; j++) {
@@ -3458,7 +3458,7 @@ Bool_t mainSelector::Process(Long64_t entry)
            if ((TrigObj_filterBits[i] & BIT(j)) == BIT(j)) h_Z_muo0_trig->Fill(j+0.5, weight_Z_muo);
          }
          if ((TrigObj_filterBits[i] & 2) == 2) match0 = true; // 2 = Iso
-         if ((TrigObj_filterBits[i] & 8) == 8) match0 = true; // 8 = IsoTkMu or 1mu
+         if ((TrigObj_filterBits[i] & 8) == 8) match0 = true; // 8 = 1mu
        }
        if (muo0_qcd.DeltaR(tmp_trg) < 0.1) {
          for (uint j = 0; j < 32; j++) {
