76359705ef8fbba5cb6684e8dc836bc731010b0a
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2019-08-01 08:04:42 +0200

    cosmetics

--- VggTools/macros/evtcount.sh
+++ VggTools/macros/evtcount.sh
@@ -13,6 +13,18 @@ cd $WORKDIR
 export ROOT_HIST=0
 
 for L in `ls lists/`; do
+  [ -z "${L##*WG_WJetsToLNu*}" ] && continue
+  [ -z "${L##*WGG_WJetsToLNu*}" ] && continue
+  [ -z "${L##*WTauNu_WJetsToLNu*}" ] && continue
+  [ -z "${L##*WG_WToLNu*}" ] && continue
+  [ -z "${L##*WGG_WToLNu*}" ] && continue
+  [ -z "${L##*WTauNu_WToLNu*}" ] && continue
+  [ -z "${L##*ZG_DYJetsToLL*}" ] && continue
+  [ -z "${L##*ZGG_DYJetsToLL*}" ] && continue
+  [ -z "${L##*ZTauTau_DYJetsToLL*}" ] && continue
+  [ -z "${L##*ZG_DYToLL*}" ] && continue
+  [ -z "${L##*ZGG_DYToLL*}" ] && continue
+  [ -z "${L##*ZTauTau_DYToLL*}" ] && continue
   root-6.12 -l -b -q evtcount.C\(\"lists/$L\"\) | \
   grep -v 'Processing'
 done
