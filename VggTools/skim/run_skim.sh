1923d188acd4639f533d1911b0f640122a4c53a6
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2019-10-13 21:54:19 +0200

    add helper script for skimming

---
+++ VggTools/skim/run_skim.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+WORKDIR=$HOME/work/cms/VggAnalysis/VggTools/skim
+cd $WORKDIR
+
+busers
+echo
+echo -n '/eos/infnts/cms/store/data/ | grep Nano1June2019 -> '
+eos find -f /eos/infnts/cms/store/data/ | grep Nano1June2019 | wc -l
+echo -n '/eos/infnts/cms/store/user/dellaric/data/ | grep Nano1June2019 -> '
+eos find -f /eos/infnts/cms/store/user/dellaric/data/ | grep Nano1June2019 | wc -l
+echo -n './logs/ -> '
+find logs/ -type f -name '*_Skim.log' | wc -l
+echo
+./sub_data_skim.sh
+echo
+./check_logs.sh
+
+exit
