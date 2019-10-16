15240f1bb0103be9586a6c7260ee91e98b291cac
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2019-10-16 11:47:40 +0200

    drop unused scripts

--- VggTools/macros/copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-[ $# -lt 2 ] && exit
-
-cp -rp -v data/$1 data/$2
-
-cp -rp -v html/$1 html/$2
-
-exit
--- VggTools/macros/rename.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-[ $# -lt 2 ] && exit
-
-mv -v data/$1 data/$2
-
-mv -v html/$1 html/$2
-
-exit
