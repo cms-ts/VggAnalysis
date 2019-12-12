aba98d6263473161dcd267615321c73dfb21ef5c
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2019-12-12 10:35:33 +0100

    enable "qcd_fit" systematics

--- VggTools/macros/sub_process.sh
+++ VggTools/macros/sub_process.sh
@@ -86,7 +86,7 @@ if [ ! -z "$4" ]; then
     FLAGS=$FLAGS" jet_misid_iso1"
     FLAGS=$FLAGS" jet_misid_iso2"
     FLAGS=$FLAGS" jet_bkg_mc"
-#    FLAGS=$FLAGS" qcd_fit"
+    FLAGS=$FLAGS" qcd_fit"
   else
     FLAGS=$4
   fi
