3a8ecd4a46378b6eda9cd540a3847628148b5809
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2020-04-15 11:27:39 +0200

    cleanup of skimming scripts

--- VggTools/skim/job_data_skim.sh
+++ VggTools/skim/job_data_skim.sh
@@ -14,7 +14,7 @@ fi
 
 if [ -z "${1##*Run2016*02Apr2020*}" ]; then
   python nano_postproc.py \
-  -P -J /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt \
+  -J /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt \
   . $F > logs/$G.log 2>&1
   eos cp $G.root $D/$G.root >> logs/$G.log 2>&1
   rm $G.root >> logs/$G.log 2>&1
@@ -22,7 +22,7 @@ fi
 
 if [ -z "${1##*Run2017*02Apr2020*}" ]; then
   python nano_postproc.py \
-  -P -J /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt \
+  -J /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt \
   . $F > logs/$G.log 2>&1
   eos cp $G.root $D/$G.root >> logs/$G.log 2>&1
   rm $G.root >> logs/$G.log 2>&1
@@ -30,7 +30,7 @@ fi
 
 if [ -z "${1##*Run2018*02Apr2020*}" ]; then
   python nano_postproc.py \
-  -P -J /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt \
+  -J /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt \
   . $F > logs/$G.log 2>&1
   eos cp $G.root $D/$G.root >> logs/$G.log 2>&1
   rm $G.root >> logs/$G.log 2>&1
--- VggTools/skim/nano_postproc.py
+++ VggTools/skim/nano_postproc.py
@@ -2,52 +2,22 @@
 import os, sys
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
-from importlib import import_module
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
 
 if __name__ == "__main__":
     from optparse import OptionParser
     parser = OptionParser(usage="%prog [options] outputDir inputFiles")
-    parser.add_option("-s", "--postfix",dest="postfix", type="string", default=None, help="Postfix which will be appended to the file name (default: _Friend for friends, _Skim for skims)")
     parser.add_option("-J", "--json",  dest="json", type="string", default=None, help="Select events using this JSON file")
-    parser.add_option("-c", "--cut",  dest="cut", type="string", default=None, help="Cut string")
-    parser.add_option("-b", "--branch-selection",  dest="branchsel", type="string", default=None, help="Branch selection")
-    parser.add_option("--bi", "--branch-selection-input",  dest="branchsel_in", type="string", default=None, help="Branch selection input")
-    parser.add_option("--bo", "--branch-selection-output",  dest="branchsel_out", type="string", default=None, help="Branch selection output")
-    parser.add_option("--friend",  dest="friend", action="store_true", default=False, help="Produce friend trees in output (current default is to produce full trees)")
-    parser.add_option("--full",  dest="friend", action="store_false",  default=False, help="Produce full trees in output (this is the current default)")
-    parser.add_option("--noout",  dest="noOut", action="store_true",  default=False, help="Do not produce output, just run modules")
-    parser.add_option("--justcount",   dest="justcount", default=False, action="store_true",  help="Just report the number of selected events") 
-    parser.add_option("-I", "--import", dest="imports",  type="string", default=[], action="append", nargs=2, help="Import modules (python package, comma-separated list of ");
-    parser.add_option("-z", "--compression",  dest="compression", type="string", default=("LZMA:9"), help="Compression: none, or (algo):(level) ")
-    parser.add_option("-P", "--provenance",  dest="provenance", default=False, action="store_true",  help="Save provenance")
 
     (options, args) = parser.parse_args()
-
-    if options.friend:
-        if options.cut or options.json: raise RuntimeError("Can't apply JSON or cut selection when producing friends")
 
     if len(args) < 2 :
 	 parser.print_help()
          sys.exit(1)
-    outdir = args[0]; args = args[1:]
 
-    modules = []
-    for mod, names in options.imports: 
-        import_module(mod)
-        obj = sys.modules[mod]
-        selnames = names.split(",")
-        for name in dir(obj):
-            if name[0] == "_": continue
-            if name in selnames:
-                print "Loading %s from %s " % (name, mod)
-                modules.append(getattr(obj,name)())
-    if options.noOut:
-        if len(modules) == 0: 
-            raise RuntimeError("Running with --noout and no modules does nothing!")
-    if options.branchsel!=None:
-        options.branchsel_in = options.branchsel
-        options.branchsel_out = options.branchsel
-    p=PostProcessor(outdir,args,options.cut,options.branchsel_in,modules,options.compression,options.friend,options.postfix,options.json,options.noOut,options.justcount,options.provenance,outputbranchsel= options.branchsel_out)
+    outdir = args[0]
+    args = args[1:]
+
+    p=PostProcessor(outdir, args, jsonInput = options.json, provenance = True)
     p.run()
 
