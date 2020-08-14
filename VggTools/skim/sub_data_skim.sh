9c50b7901a037ea32d16fa9dd19984384874a55b
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2020-08-14 12:13:56 +0200

    cleanup

--- VggTools/skim/sub_data_skim.sh
+++ VggTools/skim/sub_data_skim.sh
@@ -20,27 +20,7 @@ cd $WORKDIR
 
 DATADIR=/eos/infnts/cms/store/data
 
-for F in `eos find -f $DATADIR/ | grep Run2016 | grep NANOAOD | grep 02Apr2020`; do
-  if [ -s $F ]; then
-    if [ ! -e logs/`basename $F .root`_Skim.log ]; then
-      touch logs/`basename $F .root`_Skim.log
-      eos mkdir -p `dirname $F | sed -e 's;data;user/dellaric/data;'`
-      bsub -q $QUEUE -R "$EXCLUDED_HOSTS" -e /dev/null -o /dev/null job_data_skim.sh $F
-    fi
-  fi
-done
-
-for F in `eos find -f $DATADIR/ | grep Run2017 | grep NANOAOD | grep 02Apr2020`; do
-  if [ -s $F ]; then
-    if [ ! -e logs/`basename $F .root`_Skim.log ]; then
-      touch logs/`basename $F .root`_Skim.log
-      eos mkdir -p `dirname $F | sed -e 's;data;user/dellaric/data;'`
-      bsub -q $QUEUE -R "$EXCLUDED_HOSTS" -e /dev/null -o /dev/null job_data_skim.sh $F
-    fi
-  fi
-done
-
-for F in `eos find -f $DATADIR/ | grep Run2018 | grep NANOAOD | grep 02Apr2020`; do
+for F in `eos find -f $DATADIR/ | grep NANOAOD | grep 02Apr2020`; do
   if [ -s $F ]; then
     if [ ! -e logs/`basename $F .root`_Skim.log ]; then
       touch logs/`basename $F .root`_Skim.log
