756ff904ff8ef31d871ca17a40972624a3aa3aa7
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2020-08-31 20:24:58 +0200

    use LSF host groups in auto_pu

--- VggTools/macros/sub_auto_pu.sh
+++ VggTools/macros/sub_auto_pu.sh
@@ -3,13 +3,17 @@
 QUEUE=normal
 export USE_LSF_STARTER=no
 
-EXCLUDED_HOSTS=""
-EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm003 && hname!=farm004 && hname!=farm005 && hname!=farm006 && "
-EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm019 && hname!=farm020 && hname!=farm021 && hname!=farm022 && "
-EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm025 && hname!=farm026 && "
-EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm045 && hname!=farm046 && "
-EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm061 && hname!=farm062 && hname!=farm063 && hname!=farm064 && "
-EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=farm073 && hname!=farm074 && hname!=farm075 && hname!=farm076"
+EXCLUDED_GROUPS="ts-acid_hg cfarmts_hg"
+
+for GROUP in $EXCLUDED_GROUPS; do
+  for HOST in `bmgroup -r $GROUP`; do
+    if [ -z "${HOST##*farm0*}" ]; then
+      EXCLUDED_HOSTS=$EXCLUDED_HOSTS"hname!=$HOST "
+    fi
+  done
+done
+
+EXCLUDED_HOSTS=`echo $EXCLUDED_HOSTS | sed -e 's/ / \&\& /g'`
 
 WORKDIR=$HOME/work/cms/VggAnalysis/VggTools/macros
 cd $WORKDIR
